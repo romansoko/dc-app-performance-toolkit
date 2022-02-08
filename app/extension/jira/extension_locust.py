9db6299dba03e79a52745a73d524e759c53c504a
romansoko <69453555+romansoko@users.noreply.github.com>
2022-02-08 14:28:31 +0200

    Update extension_locust.py

--- app/extension/jira/extension_locust.py
+++ app/extension/jira/extension_locust.py
@@ -1,12 +1,29 @@
-import re
 from locustio.common_utils import init_logger, jira_measure, run_as_specific_user  # noqa F401
+from util.api.jira_clients import JiraRestClient
+from util.conf import JIRA_SETTINGS
+
 
 logger = init_logger(app_type='jira')
 
+client = JiraRestClient(JIRA_SETTINGS.server_url, JIRA_SETTINGS.admin_login, JIRA_SETTINGS.admin_password)
+sync_url = "/plugins/servlet/ws-plugin?sync"
+
 
 @jira_measure("locust_app_specific_action")
-# @run_as_specific_user(username='admin', password='admin')  # run as specific user
+@run_as_specific_user(username=client.user, password=client.password)  # run as specific user
 def app_specific_action(locust):
+    ws_title_page = "ws-plugin jira - "
+    logger.info("Executing ws sync")
+    resp = locust.get(sync_url, catch_response=True)
+    content = resp.content.decode('utf-8')
+    if not (f'title=" {ws_title_page}' in content):
+        logger.error(f'sync didnt work properly\nresponse: {resp}\nplease examine the content{content}')
+    assert f'title=" {ws_title_page}' in content, 'ws sync failed'
+
+
+
+    ################ TOOLKIT EXAMPLE ###############
+    """
     r = locust.get('/app/get_endpoint', catch_response=True)  # call app-specific GET endpoint
     content = r.content.decode('utf-8')   # decode response content
 
@@ -27,3 +44,4 @@ def app_specific_action(locust):
     if 'assertion string after successful POST request' not in content:
         logger.error(f"'assertion string after successful POST request' was not found in {content}")
     assert 'assertion string after successful POST request' in content  # assertion after POST request
+    """
